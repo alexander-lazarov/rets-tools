50f2f92e36298acc1c86b4e6586c7eb78fd192bb
Alexander Lazarov <alexander.lazaroff@gmail.com>
2015-10-27 16:07:06 +0200

    make ConfigLoader return PHRETS\Configuration

--- src/Util/ConfigLoader.php
+++ src/Util/ConfigLoader.php
@@ -2,16 +2,35 @@
 
 namespace RetsTools\Util;
 use Symfony\Component\Yaml\Parser;
+use PHRETS\Configuration;
 
 class ConfigLoader
 {
     protected static $PATH = __DIR__.'/../../';
 
-    public static function get($configFilename = 'rets.yml')
+    protected static function getRawConfig($configFilename)
     {
         $parser = new Parser;
         $configFileContents = file_get_contents(self::$PATH.$configFilename);
 
         return $parser->parse($configFileContents)['rets'];
+    }
+
+    public static function get($configFilename = 'rets.yml')
+    {
+        $c = self::getRawConfig($configFilename);
+        $retsConfig = new Configuration;
+
+        $retsConfig->setLoginUrl($c['url'])
+                   ->setUsername($c['username'])
+                   ->setPassword($c['password'])
+                   ->setRetsVersion($c['rets_version'])
+                   ->setUserAgent($c['user_agent'])
+                   ->setUserAgentPassword($c['user_agent_password'])
+                   ->setHttpAuthenticationMethod($c['http_auth_method'])
+                   ->setOption('use_post_method', $c['use_post_method'])
+                   ->setOption('disable_follow_location', $c['disable_follow_location']);
+
+        return $retsConfig;
     }
 }
